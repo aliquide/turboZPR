160dfc8e1a1ca49b0a9da2cd51242f825e50ba15
aliquide <ihuarax@gmail.com>
2014-03-22 12:44:21 +0100

    Dodawanie dynamiczne kart

--- Eitistone/html/script.js
+++ Eitistone/html/script.js
@@ -1,5 +1,28 @@
 $(document).ready(function() {
 
-  $(".draggable").draggable();
+  function Card (name, image, description, attack, health) {
+  	this.name = name;
+  	this.image = image;
+  	this.description = description;
+  	this.attack = attack;
+  	this.health = health;
+  }
+
+  var monsterCard = new Card("Smoczysko", "images/monster1.jpg", "Straszny potwór", 6, 5);
+
+  Card.prototype.displayCard = function() {
+  	var htmlCard = "<div class=\"draggable\"><div class=\"card\">"
+	htmlCard += "<h1 class=\"monsterName\">" + this.name + "</h1>"
+	htmlCard += "<div class=\"monsterImgBorder\"><img class=\"monsterImg\" src=\"" + this.image + "\"></div>"
+	htmlCard += "<div class=\"monsterDescription\">" + this.description + "</div>"
+	htmlCard += "<div class=\"attackField\">" + this.attack + "</div>"
+	htmlCard += "<div class=\"healthField\">" + this.health + "</div>"
+	htmlCard += "</div></div>"
+
+	$("body").append(htmlCard);
+  	$(".draggable").draggable({stack: "div"});
+  }
+
+  $("#addCard").click(function () { monsterCard.displayCard(); });
 
 });
