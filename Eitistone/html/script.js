413ab35629ec9654a9e055b4f98c7beb0cb867f9
aliquide <ihuarax@gmail.com>
2014-03-27 01:01:16 +0100

    + pozycjonowanie kart na początku w ręce gracza + karty wracają na swoje miejsce, chyba że się je położy na stole

--- Eitistone/html/script.js
+++ Eitistone/html/script.js
@@ -8,20 +8,33 @@ $(document).ready(function() {
   	this.health = health;
   }
 
+  var MAX_CARDS_ON_HAND = 8;
+  var MAX_CARDS_ON_BOARD = 5;
+
   var monsterCard = new Card("Smoczysko", "images/monster1.jpg", "Straszny potwór", 6, 5);
 
-  Card.prototype.displayCard = function() {
-  	var htmlCard = "<div class=\"draggable\"><div class=\"card\">"
-  	htmlCard += "<h1 class=\"monsterName\">" + this.name + "</h1>"
-  	htmlCard += "<div class=\"monsterImgBorder\"><img class=\"monsterImg\" src=\"" + this.image + "\"></div>"
-  	htmlCard += "<div class=\"monsterDescription\">" + this.description + "</div>"
-  	htmlCard += "<div class=\"attackField\">" + this.attack + "</div>"
-  	htmlCard += "<div class=\"healthField\">" + this.health + "</div>"
-  	htmlCard += "</div></div>"
+  $("#cardsPlayerBoard").droppable();
 
-  	$("#playerHand").append(htmlCard);
-  	$(".draggable").draggable({stack: "div"});
+  Card.prototype.displayCard = function(x) {
+
+    if(($("#playerCardList li").size() != MAX_CARDS_ON_HAND && x === 1 ) || ($("#opponentCardList li").size() != MAX_CARDS_ON_HAND && x === 2 )) {
+
+      var htmlCard = "<li class=\"draggable\"><div><div class=\"card\">"
+      htmlCard += "<h1 class=\"monsterName\">" + this.name + "</h1>"
+      htmlCard += "<div class=\"monsterImgBorder\"><img class=\"monsterImg\" src=\"" + this.image + "\"></div>"
+      htmlCard += "<div class=\"monsterDescription\">" + this.description + "</div>"
+      htmlCard += "<div class=\"attackField\">" + this.attack + "</div>"
+      htmlCard += "<div class=\"healthField\">" + $("#playerCardList li").size() + "</div>"
+      htmlCard += "</div></div></li>"
+
+      if (x === 1)
+  	   $("#playerCardList").append(htmlCard);
+      else if (x === 2)
+       $("#opponentCardList").append(htmlCard);
+    }
+  	$(".draggable").draggable({stack: "div", revert: "invalid"});
   }
 
-  $("#addCard").click(function () { monsterCard.displayCard(); });
+  $("#addCard1").click(function () { monsterCard.displayCard(1); });
+  $("#addCard2").click(function () { monsterCard.displayCard(2); });
 });
