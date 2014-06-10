d46d263a61c6994ef811dd3d8b580bdd365241df
mprewysz <m.kwinto@gmail.com>
2014-06-10 18:36:42 +0200

    skrocenie kodu

--- model/toJson.h
+++ model/toJson.h
@@ -6,43 +6,26 @@ using namespace std;
 using boost::property_tree::ptree;
 using boost::property_tree::basic_ptree;
 
+boost::property_tree::ptree addCards(std::pair<int,std::string> *el){
+        boost::property_tree::ptree pCards;
+        for(int indeks= 0; indeks<CARDS; ++indeks){
+            boost::property_tree::ptree card;
+            card.put("Id", el[indeks].first);
+            card.put("cardStr", el[indeks].second);
+            pCards.push_back(std::make_pair("",card));
+        }
+        return pCards;
+}
+
 boost::property_tree::ptree toJson(jMockUp mp){
 
     boost::property_tree::ptree pt, pCards;
 
-    for(int indeks= 0; indeks<5; ++indeks){
-        boost::property_tree::ptree name, id, card;
-        card.put("Id", mp.p1Table[indeks].first);
-        card.put("cardStr", mp.p1Table[indeks].second);
-        pCards.push_back(std::make_pair("",card));
-    }
-    pt.add_child("p1Table", pCards);
+    pt.add_child("p1Table", addCards(mp.p1Table));
+    pt.add_child("p2Table", addCards(mp.p2Table));
+    pt.add_child("p1Hand", addCards(mp.p1Hand));
+    pt.add_child("p2Hand", addCards(mp.p2Hand));
 
-    for(int indeks= 0; indeks<5; ++indeks){
-        boost::property_tree::ptree name, id, card;
-        card.put("Id", mp.p2Table[indeks].first);
-        card.put("cardStr", mp.p2Table[indeks].second);
-        pCards.push_back(std::make_pair("",card));
-    }
-
-    pt.add_child("p2Table", pCards);
-
-    for(int indeks= 0; indeks<5; ++indeks){
-        boost::property_tree::ptree name, id, card;
-        card.put("Id", mp.p1Hand[indeks].first);
-        card.put("cardStr", mp.p1Hand[indeks].second);
-        pCards.push_back(std::make_pair("",card));
-    }
-    pt.add_child("p1Hand", pCards);
-
-    for(int indeks= 0; indeks<5; ++indeks){
-        boost::property_tree::ptree name, id, card;
-        card.put("Id", mp.p2Hand[indeks].first);
-        card.put("cardStr", mp.p2Hand[indeks].second);
-        pCards.push_back(std::make_pair("",card));
-    }
-
-    pt.add_child("p2Hand", pCards);
     pt.put("p1", mp.id_player_A);
     pt.put("p2", mp.id_player_B);
     pt.put("StanGry", mp.actual_state_of_game);
