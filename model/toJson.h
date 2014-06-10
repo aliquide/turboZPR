d98297c89ca626d2d30a6261a40cd697a1806230
mprewysz <m.kwinto@gmail.com>
2014-06-10 17:43:41 +0200

    prosty make dla mnie

--- model/toJson.h
+++ model/toJson.h
@@ -12,10 +12,8 @@ boost::property_tree::ptree toJson(jMockUp mp){
 
     for(int indeks= 0; indeks<5; ++indeks){
         boost::property_tree::ptree name, id, card;
-        card.put("Id", mp.p1Table[0].first);
-        card.put("cardStr", mp.p1Table[0].second);
-       // card.put("", name);
-     // card.put("", id);
+        card.put("Id", mp.p1Table[indeks].first);
+        card.put("cardStr", mp.p1Table[indeks].second);
         p1Table.push_back(std::make_pair("",card));
     }
        pt.add_child("p1Tabl", p1Table);
